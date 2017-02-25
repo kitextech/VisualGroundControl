1aaf7fb82ee0af2d56158be799844afa50746840
GreatApe <kugel@kth.se>
2017-02-25 17:42:56 +0200

    Initial Commit

---
+++ VisualKite/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  VisualKite
+//
+//  Created by Gustaf Kugelberg on 2017-02-25.
+//  Copyright (c) 2017 Gustaf Kugelberg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
